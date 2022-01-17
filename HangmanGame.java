e0b56cabc84bf18a965eb34faaf82f52382a5a44
Sebastian Alaimo Di Loro <sebastian.alaimo.di.loro@telekom.de>
2022-01-17 12:25:15 +0100

    Created HangmanGameClass

---
+++ HangmanGame.java
@@ -0,0 +1,24 @@
+public class HangmanGame {
+    int numberOfErrors = 0;
+
+    public static void startGame() {
+        // get word from Words Class and drawGuessingDashes
+    }
+    public static void runGame() {
+        // ask user for input ? If correct input showLetters.
+        // if input wrong -> increase numberOfErrors and draw hangman
+    }
+    public static void getUserInput() {
+        // get input and check it.
+        // ask for letter or solution to word.
+    }
+    public static void checkWinOrLoose() {
+        // Loose if number of errors >= 7
+        // win if userguess matches with word
+    }
+    public static void drawHangman(int numberOfErrors) {
+        // Draw Hangman according to number of errors.
+    }
+
+
+}
