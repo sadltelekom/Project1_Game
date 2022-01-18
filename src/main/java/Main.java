1ba63e7988374e418fd45ce4f7bc55da77c5358d
Sebastian Alaimo Di Loro <sebastian.alaimo.di.loro@telekom.de>
2022-01-18 11:21:49 +0100

    Added JavaFX ???

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-       HangmanGame.startGame();
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        //HangmanGame.startGame();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hangman Game");
+
+        // setting a vBOX Layout
+        VBox box = new VBox();
+        // setting up a scene with that layout
+        Scene firstScene = new Scene(box,640,480);
+        // put scene into this stage.
+        primaryStage.setScene(firstScene);
+        primaryStage.show();
 
     }
 }
